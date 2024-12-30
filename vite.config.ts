4a9b64d6564aeea086b2a154ade52a84512417db
Salino3 <flaviobcn012@gmail.com>
2024-12-30 11:00:53 +0100

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/react-cookies-auth/",
+  base: "/react-cookies-auth-01/",
   plugins: [react()],
 });
