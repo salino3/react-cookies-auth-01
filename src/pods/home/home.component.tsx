db4a82ea365828915a79d209652f712caa991b46
Salino3 <flaviobcn012@gmail.com>
2025-01-12 08:35:21 +0100

    add useRef

--- src/pods/home/home.component.tsx
+++ src/pods/home/home.component.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import {
   CompanyFormRegister,
   FormLoginCompany,
@@ -9,27 +9,45 @@ import { Header } from "../../common-app";
 import "./home.styles.scss";
 
 export const Home: React.FC = () => {
+  const detailsRef1 = useRef<HTMLDetailsElement>(null);
+  const detailsRef2 = useRef<HTMLDetailsElement>(null);
+  const detailsRef3 = useRef<HTMLDetailsElement>(null);
+  const detailsRef4 = useRef<HTMLDetailsElement>(null);
+
+  const [refresh, setRefresh] = useState<number>(0);
+
+  const handleToggle = () => {
+    setRefresh((prev) => prev + 1);
+  };
+
   return (
     <div className="rootHomePage">
       <Header />
       <h1>Home Page</h1>
       <p>Welcome to the Home Page</p>
-      <details>
+      {/* onToggle shoots everytime property open of <details> change value  */}
+      <details name="form" ref={detailsRef1} onToggle={handleToggle}>
         <summary>Register User</summary>
-        <UserFormRegister />
+        {detailsRef1.current && detailsRef1.current.open && (
+          <UserFormRegister />
+        )}
       </details>
-      <details>
+      <details name="form" ref={detailsRef2} onToggle={handleToggle}>
         <summary>Register Company</summary>
-        <CompanyFormRegister />
+        {detailsRef2.current && detailsRef2.current.open && (
+          <CompanyFormRegister />
+        )}
       </details>
       <hr />
-      <details>
+      <details name="form" ref={detailsRef3} onToggle={handleToggle}>
         <summary>Login Users</summary>
-        <FormLoginUser />
+        {detailsRef3.current && detailsRef3.current.open && <FormLoginUser />}
       </details>
-      <details>
+      <details name="form" ref={detailsRef4} onToggle={handleToggle}>
         <summary>Login Companies</summary>
-        <FormLoginCompany />
+        {detailsRef4.current && detailsRef4.current.open && (
+          <FormLoginCompany />
+        )}
       </details>
     </div>
   );
